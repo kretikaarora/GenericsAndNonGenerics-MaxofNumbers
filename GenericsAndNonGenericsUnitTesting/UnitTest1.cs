f35db82f6868ff04011394953569a138364ef383
kretika arora <kretikaarora@gmail.com>
2020-10-20 17:28:32 +0530

    [Kretika Arora]Max of three strings numbers using non generics

--- GenericsAndNonGenericsUnitTesting/UnitTest1.cs
+++ GenericsAndNonGenericsUnitTesting/UnitTest1.cs
@@ -17,9 +17,7 @@ namespace GenericsAndNonGenericsUnitTesting
             [TestMethod]
             [DataRow(50, 20, 30)]
             [DataRow(20, 50, 30)]
-            [DataRow(20, 30, 50)]
-            [DataRow(50, 50, 50)]
-            
+            [DataRow(20, 30, 50)]          
             public void TestOfMaxOfThreeNumbersWithIntegers(int firstNumber, int secondNumber, int thirdNumber)
             {
                 //Arrange
@@ -39,10 +37,8 @@ namespace GenericsAndNonGenericsUnitTesting
             /// <param name="thirdNumber"></param>
             [DataRow(50.1f, 20.8f, 30.5f)]
             [DataRow(20.8f, 50.1f, 30.5f)]
-            [DataRow(20.8f, 30.5f, 50.1f)]
-            [DataRow(50.1f, 50.1f, 50.1f)]
+            [DataRow(20.8f, 30.5f, 50.1f)]           
             [TestMethod]
-
              public void TestOfMaxOfThreeNumbersWithFloatValues(float firstNumber, float secondNumber, float thirdNumber)
             {
                 //Arrange
@@ -52,7 +48,27 @@ namespace GenericsAndNonGenericsUnitTesting
                 //Assert
                 Assert.AreEqual(expected, actual);
             }
-
+        /// <summary>
+        /// Testing Of Max Of Three Numbers using Non Generic Method
+        /// TC 3.1 , TC 3.2 , TC 3.3
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        [TestMethod]
+        [DataRow("50", "20", "30")]
+        [DataRow("20", "50", "30")]
+        [DataRow("20", "30", "50")]
+        public void TestOfMaxOfThreeNumbersWithString(string firstNumber, string secondNumber, string thirdNumber)
+        {
+            //Arrange
+            string  expected = "50";
+            //Act          
+            string actual = TestMaximum.MaxOfThreeStrings(firstNumber, secondNumber, thirdNumber);
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+
+    }
     }
 
