2557cca96ea62eb270ac00688692eb5f0d7adee5
kretika arora <kretikaarora@gmail.com>
2020-10-20 16:53:43 +0530

    [Kretika Arora]Max of three  integer numbers using non generics

---
+++ GenericsAndNonGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsAndNonGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum of Three Numbers");
+        }
+    }
+}
