390283de3961b0a084d392a8265610a9b5255200
kretikaarora <71806050+kretikaarora@users.noreply.github.com>
2020-10-21 17:31:08 +0530

    Adding Comments

--- GenericsAndNonGenerics/Program.cs
+++ GenericsAndNonGenerics/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kretika Arora"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace UserRegistration
+using System;
 namespace GenericsAndNonGenerics
 {
     class Program
