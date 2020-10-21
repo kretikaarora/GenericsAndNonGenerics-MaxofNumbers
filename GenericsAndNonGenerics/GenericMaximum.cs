ef6d25d8b9378dc9fe33d693a4ba29dbf761ef20
kretikaarora <71806050+kretikaarora@users.noreply.github.com>
2020-10-21 17:32:21 +0530

    Adding Comments

--- GenericsAndNonGenerics/GenericMaximum.cs
+++ GenericsAndNonGenerics/GenericMaximum.cs
@@ -1,4 +1,11 @@
-﻿using System;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GenericMaximum.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kretika Arora"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace UserRegistration
+using System;
 using System.Collections.Generic;
 using System.Text;
 
